b2079794938b6765911ff0dc0d895bcfb35bbb31
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-07 16:07:52 +0100

    neetcode answer for number 94 using iteration

--- binary_tree_in_order.py
+++ binary_tree_in_order.py
@@ -5,22 +5,17 @@ from Util import create_tree_from_array
 
 
 def in_order_traversal(root: TreeNode) -> List[int]:
-    stack = [root]
+    stack = []
     elements_traversed = []
-    while len(stack) > 0:
-        current_node = stack.pop()
-        if current_node is None:
-            continue
-        left_child = current_node.left
-        right_child = current_node.right
-        current_node.left = None
-        current_node.right = None
-        if left_child is None and right_child is None:
-            elements_traversed.append(current_node.val)
-        else:
-            stack.append(right_child)
+    current_node = root
+    while current_node is not None or len(stack) > 0:
+        while current_node:
             stack.append(current_node)
-            stack.append(left_child)
+            current_node = current_node.left
+
+        current_node = stack.pop()
+        elements_traversed.append(current_node.val)
+        current_node = current_node.right
 
     return elements_traversed
 
