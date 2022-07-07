4ad708c85fca4a8b3d353b9589f21997ae6be24a
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-07 19:35:46 +0100

    solved number 101, time complexity: O(n), space complexity: O(n), where n is the number of nodes in the tree

--- symmetric_tree.py
+++ symmetric_tree.py
@@ -1,6 +1,30 @@
 """Number 101: symmetric tree"""
 from Util import TreeNode
+from Util import create_tree_from_array
 
 
 def is_symmetric(root: TreeNode) -> bool:
-    return False
+    lhs_queue = [root.left] if root.left else []
+    rhs_queue = [root.right] if root.right else []
+
+    while len(lhs_queue) > 0 and len(rhs_queue) > 0:
+        left_val = lhs_queue.pop(0)
+        right_val = rhs_queue.pop(0)
+        if left_val is not None and right_val is not None:
+            if left_val.val != right_val.val:
+                return False
+        elif left_val != right_val:
+            return False
+        if left_val is not None:
+            lhs_queue.append(left_val.left)
+            lhs_queue.append(left_val.right)
+        if right_val is not None:
+            rhs_queue.append(right_val.right)
+            rhs_queue.append(right_val.left)
+
+    return len(lhs_queue) == len(rhs_queue)
+
+
+array1 = [1, 2, 2, 3, 4, 4, 3, 5, None, None, None, None, None, None, 5]
+root_input = create_tree_from_array(array1)
+print(is_symmetric(root_input))
