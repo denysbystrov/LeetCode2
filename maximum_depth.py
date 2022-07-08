59f2ddc14210993d1ecf3ffc5f076bff062822ce
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-08 12:29:23 +0100

    implemented number 104 recursively, using DFS

--- maximum_depth.py
+++ maximum_depth.py
@@ -3,4 +3,13 @@ from Util import TreeNode
 
 
 def maximum_depth(root: TreeNode) -> int:
-    return 0
+
+    def max_depth_rec(node) -> int:
+        if node is None:
+            return 0
+
+        left_depth = max_depth_rec(node.left)
+        right_depth = max_depth_rec(node.right)
+        return 1 + max(left_depth, right_depth)
+
+    return max_depth_rec(root)
