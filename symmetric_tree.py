2c0b58e04b004b93c2418a56d49a75f7a415d497
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-08 09:42:53 +0100

    solved number 101, with inpsiration of youtube

--- symmetric_tree.py
+++ symmetric_tree.py
@@ -4,25 +4,16 @@ from Util import create_tree_from_array
 
 
 def is_symmetric(root: TreeNode) -> bool:
-    lhs_queue = [root.left] if root.left else []
-    rhs_queue = [root.right] if root.right else []
-
-    while len(lhs_queue) > 0 and len(rhs_queue) > 0:
-        left_val = lhs_queue.pop(0)
-        right_val = rhs_queue.pop(0)
-        if left_val is not None and right_val is not None:
-            if left_val.val != right_val.val:
-                return False
-        elif left_val != right_val:
+    def is_symmetric_rec(left_node: TreeNode, right_node: TreeNode):
+        if left_node is None or right_node is None:
+            return left_node == right_node
+        if left_node.val == right_node.val:
+            return is_symmetric_rec(left_node.left, right_node.right) and is_symmetric_rec(left_node.right,
+                                                                                           right_node.left)
+        else:
             return False
-        if left_val is not None:
-            lhs_queue.append(left_val.left)
-            lhs_queue.append(left_val.right)
-        if right_val is not None:
-            rhs_queue.append(right_val.right)
-            rhs_queue.append(right_val.left)
 
-    return len(lhs_queue) == len(rhs_queue)
+    return is_symmetric_rec(root.left, root.right)
 
 
 array1 = [1, 2, 2, 3, 4, 4, 3, 5, None, None, None, None, None, None, 5]
