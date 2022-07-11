95a7539df0bccdbcb340a2793d7365abfec7632e
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-11 09:47:57 +0100

    implemented number 118 my version

--- pascal_traingle.py
+++ pascal_traingle.py
@@ -3,4 +3,14 @@ from typing import List
 
 
 def generate(num_rows: int) -> List[List[int]]:
-    return [[0]]
+    pascal_traingle = [[1]]
+    while len(pascal_traingle) < num_rows:
+        num_components = len(pascal_traingle) + 1
+        new_row = []
+        for i in range(num_components):
+            left_val = 0 if i-1 < 0 else pascal_traingle[-1][i-1]
+            right_val = 0 if i >= len(pascal_traingle[-1]) else pascal_traingle[-1][i]
+            new_row.append(left_val+right_val)
+        pascal_traingle.append(new_row)
+
+    return pascal_traingle
