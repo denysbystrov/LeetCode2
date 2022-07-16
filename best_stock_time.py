abeb856badf0ae852a42bcf903b33cbf6916bb15
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-16 10:39:40 +0100

    implemented tests for number 121

---
+++ best_stock_time.py
@@ -0,0 +1,7 @@
+"""Number 121: Best time to buy and sell stock"""
+from typing import List
+
+
+def max_profit(prices: List[int]) -> int:
+    return 0
+
---
+++ test_best_stock_time.py
@@ -0,0 +1,20 @@
+import pytest
+from best_stock_time import max_profit
+
+
+use_cases = (
+    ([7, 1, 3, 4, 7, 2, 5], 6),
+    ([4, 3, 2, 1, 0], 0)
+)
+
+edge_cases = (
+    ([1], 0),
+    ([1, 2], 1),
+    ([2, 1], 0)
+)
+
+
+@pytest.mark.parametrize(('input_prices', 'expected'), use_cases+edge_cases)
+def test_max_profit(input_prices, expected):
+    assert max_profit(input_prices) == expected
+
