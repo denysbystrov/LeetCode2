94b3edace32aaa900c8e5f0e587b0664808cb238
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-16 13:51:44 +0100

    My second attempt at the solution, however it is still O(n^2)

--- best_stock_time.py
+++ best_stock_time.py
@@ -3,13 +3,30 @@ from typing import List
 
 
 def max_profit(prices: List[int]) -> int:
-    highest_profit = 0
-    for i in range(len(prices)):
-        for j in range(i, len(prices)):
-            if prices[j] - prices[i] > highest_profit:
-                highest_profit = prices[j] - prices[i]
+    current_pointer = 1
+    buy_pointer = 0
+    sell_pointer = 0
+    total_profit = 0
+    while current_pointer < len(prices):
+        if total_profit == 0:
+            if prices[current_pointer] > prices[buy_pointer]:
+                sell_pointer = current_pointer
+            else:
+                buy_pointer = current_pointer
+                sell_pointer = current_pointer
+        else:
+            for i in range(sell_pointer+1, current_pointer):
+                if prices[i] < prices[buy_pointer]:
+                    if prices[current_pointer] - prices[i] >= total_profit:
+                        buy_pointer = i
+                        sell_pointer = current_pointer
+            if prices[sell_pointer] <= prices[current_pointer]:
+                sell_pointer = current_pointer
 
-    return highest_profit
+        total_profit = prices[sell_pointer] - prices[buy_pointer]
+        current_pointer += 1
+
+    return total_profit
 
 
-print(max_profit([2, 1, 2, 0, 1]))
+print(max_profit([4, 3, 2, 1, 0]))
