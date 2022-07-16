34fdf2c97c812533d7e7f654a078c258bbd7cc6f
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-16 14:16:20 +0100

    Implemented tests for number 125

---
+++ test_valid_palindrome.py
@@ -0,0 +1,21 @@
+import pytest
+from valid_palindrome import is_palidrome
+
+
+use_cases = (
+    ("A man, a plan, a canal: Panama", True),
+    ('xfgddefddgfx', False),
+    ('ghf: , fdsaf', False)
+)
+
+edge_cases = (
+    (' ', True),
+    (' a :', True),
+    (':/ a/:: f', False)
+)
+
+
+@pytest.mark.parametrize(('input_s', 'expected'), use_cases+edge_cases)
+def test_is_palidrome(input_s, expected):
+    assert is_palidrome(input_s) == expected
+
---
+++ valid_palindrome.py
@@ -0,0 +1,5 @@
+"""Number 125: Valid Palidrome"""
+
+
+def is_palidrome(s: str) -> bool:
+    return False
