0a2be2bb66b30d05a6abbb1162e6bac3ef1bb0b7
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-18 10:15:55 +0100

    Implemented number 141 using neetcode method

--- cycle_list.py
+++ cycle_list.py
@@ -5,14 +5,11 @@ from Util import create_list_from_array
 
 def has_cycle(head: ListNode) -> bool:
     pointer1 = head
-    pointer2 = head.next if head.next else None
-    count = 0
-    while pointer1 and pointer2:
+    pointer2 = head
+    while pointer2 and pointer2.next:
+        pointer1 = pointer1.next
+        pointer2 = pointer2.next.next
         if pointer1 == pointer2:
             return True
-        if count % 2 == 0:
-            pointer1 = pointer1.next
-        pointer2 = pointer2.next
-        count += 1
 
     return False
