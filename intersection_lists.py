8aa4e6abbd6157435db24a66508e077a099dd056
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-19 11:15:24 +0100

    Implemented neetcode version for num 160

--- intersection_lists.py
+++ intersection_lists.py
@@ -4,33 +4,14 @@ from Util import create_two_lists
 
 
 def get_intersection_node(head_a: ListNode, head_b: ListNode) -> ListNode:
-    def find_list_length(node: ListNode) -> int:
-        current_node = node
-        length = 0
-        while current_node:
-            length += 1
-            current_node = current_node.next
+    l1, l2 = head_a, head_b
+    while l1 != l2:
+        l1 = l1.next if l1 else head_b
+        l2 = l2.next if l2 else head_a
 
-        return length
-
-    length_a = find_list_length(head_a)
-    length_b = find_list_length(head_b)
-
-    while length_a != length_b:
-        if length_b > length_a:
-            head_b = head_b.next
-            length_b -= 1
-        else:
-            head_a = head_a.next
-            length_a -= 1
-
-    while head_a != head_b:
-        head_a = head_a.next
-        head_b = head_b.next
-
-    return head_a
+    return l1
 
 
-input_head_a, input_head_b, expected = create_two_lists([1, 2, 3, 4, 5], [6, 7, 8], 2)
+input_head_a, input_head_b, expected = create_two_lists([2, 6, 4], [3, 5], -1)
 print(get_intersection_node(input_head_a, input_head_b))
 
